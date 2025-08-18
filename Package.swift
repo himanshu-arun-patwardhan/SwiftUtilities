7837d59608d8da36873844ffc8a05df906c6775e
himanshu-arun-patwardhan <himanshu.arun.patwardhan@gmail.com>
2025-08-18 22:43:15 +0530

    [clean code writing]

--- Package.swift
+++ Package.swift
@@ -5,20 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUtilities",
-    platforms: [
-        .iOS(.v16)
-    ],
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUtilities",
-            targets: ["SwiftUtilities"]),
+            targets: ["SwiftUtilities"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUtilities"),
-
+            name: "SwiftUtilities"
+        )
     ]
 )
